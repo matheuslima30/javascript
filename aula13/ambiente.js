a72c027ba257e2c09de35250e24ff00c47593ccd
matheuslima30 <120588843+matheuslima30@users.noreply.github.com>
2023-09-04 16:04:10 -0300

    comecei aula 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 10)
+
+
+/*
+var c = 1
+while (c <= 3) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
