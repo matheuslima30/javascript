9e1ee6436d3fcb32248650aba44bf2f11ea5f4ce
matheuslima30 <120588843+matheuslima30@users.noreply.github.com>
2023-12-11 12:52:45 -0300

    projeto jogo da velha

---
+++ Web/jogo-da-velha/assets/js/scripts.js
@@ -0,0 +1,48 @@
+const cellElements = document.querySelectorAll("[data-cell");
+const board = document.querySelector("[data-board")
+
+let isCircleTurn;
+
+const startGame = () => {
+    for (const cell of cellElements) {
+        cell.addEventListener('click', handleClick, { once: true });
+    }
+
+    isCircleTurn = false;
+
+    board.classList.add('x')
+}
+    
+
+const placeMark = (cell, classToAdd) => {
+    cell.classList.add(classToAdd);
+};
+
+const swapTurns = () => {
+    isCircleTurn = !isCircleTurn
+
+    board.classList.remove('circle')
+    board.classList.remove('x')
+
+    if (isCircleTurn) {
+        board.classList.add('circle')
+    } else {
+        board.classList.add('x')
+    }
+};
+
+const handleClick = (e) => {
+    // Colocar a marca (x ou circulo)
+    const cell = e.target
+    const classToAdd = isCircleTurn ? 'circle' : 'x';
+
+    placeMark(cell, classToAdd)
+    // Verificar por vitória
+
+    // Verificar por empate
+
+    // Mudar símbolo
+    swapTurns()
+};
+
+startGame()
